4e32efb55b0ba7938c3b654b60fb6492b2761429
TaeHyun Kim <taehyun.kim1982@gmail.com>
2021-03-17 10:37:50 +0900

    'Roman to Integer' solution added.

---
+++ Easy/004_ Roman to Integer/Roman to Integer/Solution.swift
@@ -0,0 +1,48 @@
+//
+//  Solution.swift
+//  Roman to Integer
+//
+//  Created by taehyun on 2021/03/17.
+//
+
+import Foundation
+
+class Solution {
+    func romanToInt(_ s: String) -> Int {
+        let arr = s.map { String($0) }  // 문자 배열
+        var answer = 0
+
+        for i in 0 ..< arr.count-1 {
+            if decode(arr[i]) < decode(arr[i+1]) {
+                answer -= decode(arr[i])
+            } else {
+                answer += decode(arr[i])
+            }
+        }
+
+        answer += decode(arr[arr.count-1])
+
+        return answer
+    }
+
+    func decode(_ roman: String) -> Int {
+        switch roman {
+        case "I":
+            return 1
+        case "V":
+            return 5
+        case "X":
+            return 10
+        case "L":
+            return 50
+        case "C":
+            return 100
+        case "D":
+            return 500
+        case "M":
+            return 1000
+        default:
+            return 0
+        }
+    }
+}
---
+++ Easy/004_ Roman to Integer/Roman to Integer/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Roman to Integer
+//
+//  Created by taehyun on 2021/03/17.
+//
+
+import Foundation
+
+let solution = Solution()
+    
+print(solution.romanToInt("III"))
+print(solution.romanToInt("IV"))
+print(solution.romanToInt("IX"))
+print(solution.romanToInt("LVIII"))
+print(solution.romanToInt("MCMXCIV"))
